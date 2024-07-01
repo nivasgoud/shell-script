97bc0134aab661b5b8fd10346dd58e80fb4de47c
nivasgoud <nivaswl12@gmail.com>
2024-07-01 21:00:44 +0530

    values to the variables as hidden format

---
+++ variable-encrypt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter the username::"
+
+read -s USERNAME  #the value you entered will be stored in to the given variable"
+
+echo "Please enter the password::"
+
+read -s PASSWORD #this will hide the password you enetered
+
+echo "Your username is $USERNAME and password you entered is $PASSWORD"
