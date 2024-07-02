b24ae2db2f171d814c8857e34d5cba2ff46d3673
nivasgoud <nivaswl12@gmail.com>
2024-07-02 16:12:05 +0530

    arrays concept group of values assign to a  single variable

---
+++ arrays.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "array concept"
+
+Fruits=( "Apple" "Banana" "Mango" )
+
+echo "First element: ${Fruits[0]}"
+
+echo "Second element: ${Fruits[1]}"
+
+echo "Third element: ${Fruits[2]}"
+
+
+echo "Total elements: ${Fruits[@]}"
