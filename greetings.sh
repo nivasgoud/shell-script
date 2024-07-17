5231ef65e6c038687ba05df99afba4b0a945ce9b
nivasgoud <nivaswl12@gmail.com>
2024-07-17 16:56:39 +0530

    updated the greeting script for command line arguments

--- greetings.sh
+++ greetings.sh
@@ -3,4 +3,38 @@
 NAME=$1
 WISHES=$2
 
+
+USAGE(){
+
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo "-n, Specify the name (mandatory)"
+    echo "-w, Specify the wishes"
+    echo "-h, Display help and exit"
+}
+
+while getopts ":n:w:h" opt
+do
+  case $opt in
+      
+      n) NAME="$OPTARG";;
+      w) WISHES="$OPTARG";;
+      h) USAGE; exit;;
+      
+  esac
+done
+
+if [ -z $NAME ] || [ -z $WISHES ]
+
+then
+
+    echo "Both -n and -w are mandatory options"
+    
+    USAGE
+
+    exit 1
+
+fi
+
+
 echo "Hello $NAME.  $WISHES  I have been learning Shell Script."
